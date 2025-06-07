dba313ff1155746ffdf1a07b7b4ad42170ed9d9f
Aaveg Tomar <tomaraaveg@gmail.com>
2025-06-07 09:44:33 +0530

    setUp redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
-import { useState } from 'react'
-import Home from './components/home/Home'
-import CourseCard from './utils/courseCard/CourseCard'
-function App() {
-  return (
-    <>
-     <div>
-     <Home/>
-     </div>
-    </>
-  )
-}
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setUser, clearUser } from './redux/userSlice';
 
-export default App
+const App = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user.user);
+
+  const handleSetUser = () => {
+    dispatch(setUser({ name: 'John', age: 30 }));
+  };
+
+  const handleClearUser = () => {
+    dispatch(setUser({}));
+  };
+
+  console.log("user " , user)
+
+ 
+
+  return (
+    <div>
+  <h2>User Info</h2>
+
+  {user && Object.keys(user).length > 0 ? (
+    <div>
+      <p>Name: {user.name}</p>
+      <p>Age: {user.age}</p>
+    </div>
+  ) : (
+    <p>No user info</p>
+  )}
+
+  <button onClick={handleSetUser}>Set User</button>
+  <button onClick={handleClearUser}>Clear User</button>
+</div>
+  );
+};
+
+export default App;
